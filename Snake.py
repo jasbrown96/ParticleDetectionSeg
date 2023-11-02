39ee9a5b8c547d0e1de2863e752789feef0ffb45
jasbrown96 <jasbrown96@gmail.com>
2023-11-01 21:27:25 -0700

    upload

---
+++ Snake.py
@@ -0,0 +1,138 @@
+# https://www.mathworks.com/matlabcentral/fileexchange/28149-snake-active-contour
+# https://scikit-image.org/docs/stable/auto_examples/edges/plot_active_contours.html
+
+from skimage.filters import gaussian
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+from skimage.filters import sobel
+from skimage.util import img_as_float
+from skimage._shared.utils import _supported_float_type
+
+def GetContourNormals2D(xt,yt, a=4):
+    # Extract x and y coordinates
+    #xt = P[:, 0]
+    #yt = P[:, 1]
+
+    # Derivatives of contour
+    n = len(xt)
+    f = (np.arange(n) + a) % n
+    b = (np.arange(n) - a) % n
+
+    dx = xt[f] - xt[b]
+    dy = yt[f] - yt[b]
+
+    # Normals of contour points
+    l = np.sqrt(dx**2 + dy**2)
+    nx = -dy / l
+    ny = dx / l
+
+    # Create the N matrix with contour normals
+    #N = np.column_stack((nx, ny))
+
+    return nx,ny
+
+
+def snake(image, snake, alpha=0.01, beta=0.1, w_line=0, w_edge=1,  gamma=0.01,
+                   max_px_move=1.0,max_num_iter=2500, convergence=0.1, delta = 0.01):
+
+
+  image = gaussian(image, 3, preserve_range=False)
+
+  img = img_as_float(image)
+  float_dtype = _supported_float_type(image.dtype)
+  img = img.astype(float_dtype, copy=False)
+
+  convergence_order = 10
+  RGB = img.ndim == 3
+
+  #Assume Grayscale
+  if w_edge != 0:
+      if RGB:
+          edge = [sobel(img[:, :, 0]), sobel(img[:, :, 1]),
+                  sobel(img[:, :, 2])]
+      else:
+          edge = [sobel(img)]
+  else:
+      edge = [0]
+
+  if RGB:
+      img = w_line*np.sum(img, axis=2) \
+          + w_edge*sum(edge)
+  else:
+      img = w_line*img + w_edge*edge[0]
+
+  # Interpolate for smoothness:
+  intp = RectBivariateSpline(np.arange(img.shape[1]),
+                              np.arange(img.shape[0]),
+                              img.T, kx=2, ky=2, s=0)
+
+  snake_xy = snake[:, ::-1]
+  x = snake_xy[:, 0].astype(float_dtype)
+  y = snake_xy[:, 1].astype(float_dtype)
+  n = len(x)
+  xsave = np.empty((convergence_order, n), dtype=float_dtype)
+  ysave = np.empty((convergence_order, n), dtype=float_dtype)
+
+  # Build snake shape matrix for Euler equation in double precision
+  eye_n = np.eye(n, dtype=float)
+  a = (np.roll(eye_n, -1, axis=0)
+        + np.roll(eye_n, -1, axis=1)
+        - 2 * eye_n)  # second order derivative, central difference
+  b = (np.roll(eye_n, -2, axis=0)
+        + np.roll(eye_n, -2, axis=1)
+        - 4 * np.roll(eye_n, -1, axis=0)
+        - 4 * np.roll(eye_n, -1, axis=1)
+        + 6 * eye_n)  # fourth order derivative, central difference
+  A = -alpha * a + beta * b
+  # Only one inversion is needed for implicit spline energy minimization:
+  inv = np.linalg.inv(A + gamma * eye_n)
+  # can use float_dtype once we have computed the inverse in double precision
+  inv = inv.astype(float_dtype, copy=False)
+
+  for i in range(max_num_iter):
+      # RectBivariateSpline always returns float64, so call astype here
+      fx = intp(x, y, dx=1, grid=False).astype(float_dtype, copy=False)
+      fy = intp(x, y, dy=1, grid=False).astype(float_dtype, copy=False)
+
+      nx,ny = GetContourNormals2D(x,y)
+
+      xn = inv @ (gamma*x + fx + delta*nx)
+      yn = inv @ (gamma*y + fy + delta*ny)
+
+      # Movements are capped to max_px_move per iteration:
+      dx = max_px_move * np.tanh(xn - x)
+      dy = max_px_move * np.tanh(yn - y)
+
+      x += dx
+      y += dy
+
+      # Convergence criteria needs to compare to a number of previous
+      # configurations since oscillations can occur.
+      j = i % (convergence_order + 1)
+      if j < convergence_order:
+          xsave[j, :] = x
+          ysave[j, :] = y
+      else:
+          dist = np.min(np.max(np.abs(xsave - x[None, :])
+                                + np.abs(ysave - y[None, :]), 1))
+          if dist < convergence:
+              break
+  return np.stack([y, x], axis=1)
+
+
+# import cv2 as cv
+# import matplotlib.pyplot as plt
+# im_dir = '/content/drive/MyDrive/Washers/Experiments/Tifs for Jason - uploaded 1017/Multi-class images (for later)/230313 Tm month block 0.1/Tm_bf002_35.1_BF.tif'
+# im = cv.imread(im_dir)
+# im = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+# image = im[285:605,380:700]
+
+# x = 100*np.cos(np.linspace(0,2*np.pi,400))+150
+# y = 100*np.sin(np.linspace(0,2*np.pi,400))+150
+# P = np.vstack([x[:-1],y[:-1]]).T
+
+# P2 = snake(image,P,alpha=0.03, beta=10, gamma=0.001, delta=0.0025)
+
+# plt.imshow(image)
+# plt.plot(P2[:,1],P2[:,0], c='r')
+# plt.show()
---
+++ Template.py
@@ -0,0 +1,295 @@
+from sklearn.metrics import pairwise_distances
+
+import time
+
+from skimage.color import rgb2gray
+from skimage import data, color
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.feature import canny
+from skimage.draw import circle_perimeter
+
+from scipy import ndimage
+from scipy.signal import find_peaks
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+im_dir = '/content/drive/MyDrive/Washers/Experiments/Tifs for Jason - uploaded 1017/Multi-class images (for later)/230313 Tm month block 0.1/Tm_bf002_35.1_BF.tif'
+im = cv2.imread(im_dir)
+im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+#plt.imshow(im)
+img_ref = im[285:605,380:700]
+#plt.imshow(img_ref)
+
+def get_ref(img_ref, n_angles=10, plot=False):
+
+  s = np.linspace(0, 2*np.pi, 400)
+  r = 150 + 200*np.sin(s)
+  c = 150 + 200*np.cos(s)
+  init = np.array([r, c]).T
+
+  snake = active_contour(gaussian(img_ref, 3, preserve_range=False),
+                        init, alpha=0.025, beta=10, gamma=0.001) #alpha=0.015
+
+  if plot:
+    plt.imshow(img_ref)
+    fig, ax = plt.subplots(figsize=(7, 7))
+    ax.imshow(img_ref, cmap=plt.cm.gray)
+    ax.plot(init[:, 1], init[:, 0], '--r', lw=3)
+    ax.scatter(snake[:, 1], snake[:, 0], c=np.arange(len(snake)), lw=3)
+    ax.set_xticks([]), ax.set_yticks([])
+    ax.axis([0, img_ref.shape[1], img_ref.shape[0], 0])
+
+    plt.scatter(np.mean(snake,axis=0)[1], np.mean(snake,axis=0)[0])
+    plt.show()
+
+  a = np.zeros_like(img_ref)
+  for x,y in snake:
+    a[int(x),int(y)]=1
+
+  b = []
+  snake_list=[]
+  for i in range(n_angles):
+    b.append(ndimage.rotate(a, 90//n_angles*i, reshape=True))
+  for i in range(n_angles):
+    snake_list.append(rotate_snake(snake,angle=90//n_angles*i))
+  for init in snake_list:
+    a = np.mean(init,axis=0)
+    init = init* 0.5
+    init += a - np.mean(init,axis=0)
+  return b, snake_list
+
+def rotate_snake(snake, angle):#angle degrees
+  #convert to radians.
+  angle = angle*np.pi/180
+  center = np.mean(snake,axis=0)
+  rot = np.array([[np.cos(angle),-np.sin(angle)],[np.sin(angle), np.cos(angle)]])
+  temp = (rot@(snake-center).T).T
+  return (temp+center).astype(int)
+
+def create_dir(results_dir):
+  if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
+
+def gen_hough(im, temp):
+  pad=800
+  c=np.zeros((im.shape[0]+pad,im.shape[1]+pad,len(temp)))
+  for (i, j), value in np.ndenumerate(im):
+    if value:
+      i+=pad//2
+      j+=pad//2
+      for k in range(len(temp)):
+        height, length = temp[k].shape[0], temp[k].shape[1]
+        temp1 = c[i-height//2:i+height//2, j-length//2:j+length//2,k]
+        temp2 = temp[k][height//2-height//2:height//2+height//2, length//2-length//2: length//2+length//2]
+        c[i-height//2:i+height//2, j-length//2:j+length//2,k] += temp[k][height//2-height//2:height//2+height//2, length//2-length//2: length//2+length//2]
+  return c[pad//2:-pad//2, pad//2:-pad//2]
+
+def non_max_suppression(accumulator, thresh, distance):
+  #peaks can be fed in as
+  #Returns mask for peaks
+  if len(accumulator.shape)==3:
+    x,y = np.nonzero(np.amax(accumulator,axis=2)>thresh)
+    z = np.argmax(accumulator,axis=2)
+    z = z[x,y] #x,y track the coordinate for the maximal value and z tracks the orientation/which angle of the reference is the best
+  else:
+    x,y = np.nonzero(accumulator>thresh)
+    z = None
+  if x.size == 0:    #I.e. x is empty
+    return None, None
+  feat = np.vstack((x,y)).T
+  dist = pairwise_distances(feat)
+  temp = np.amax(accumulator,axis=2)
+  mask = np.array([True for i in x])
+  for i in range(len(x)):
+    for j in range(i+1,len(x)):
+      if (dist[i,j]<distance) and (mask[i] == True) and (mask[j] == True):
+        val1, val2 = temp[x[i],y[i]], temp[x[j],y[j]]
+        if  val1 >= val2:
+          mask[j] = False
+        elif val1 < val2:
+          mask[i] = False
+  return x[mask],y[mask],z[mask]
+
+def circle_filter(pts1, pts2, dist):
+  x1,y1,z=pts1
+  x2,y2=pts2
+  mask = [True for i in x1]
+
+  for i in range(len(x1)):
+    for j in range(len(x2)):
+      if (x1[i] - x2[j])**2 + (y1[i]-y2[j])**2 <dist**2:
+        mask[i] = False
+        break
+  return x1[mask], y1[mask], z[mask]
+
+ref, snake_list = get_ref(img_ref)
+
+def detect_squares(image_dir, ref=ref, threshold=70):
+
+  im = cv2.imread(image_dir)
+  im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+  #img_blur = cv2.GaussianBlur(im, (5,5), 0)
+  #im = cv2.Canny(image=img_blur, threshold1=100, threshold2=150)
+  im = canny(im, sigma=0, low_threshold=100, high_threshold=150)
+
+  #Find squares accumulator
+  accumulator_square = gen_hough(im, ref)
+
+  #Find circle accumulator
+  #hough_radii = np.arange(110, 130, 5)
+
+  x,y,z = non_max_suppression(accumulator_square, thresh=threshold, distance=150)
+  if x is None:
+    return None, None#, accumulator_square
+
+  return x,y,z#, accumulator_square
+
+def detect_circles(image_dir, threshold=0.3):
+  im = cv2.imread(image_dir)
+  im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+  #img_blur = cv2.GaussianBlur(im, (5,5), 0)
+  #im = cv2.Canny(image=img_blur, threshold1=100, threshold2=150)
+
+  im = canny(im, sigma=0, low_threshold=100, high_threshold=150)
+
+  #Find circle accumulator
+  hough_radii = np.arange(110, 130, 5)
+  hough_res = hough_circle(im, hough_radii)
+
+  # Select the most prominent 3 circles
+  accumulator_circle, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii, min_xdistance=150, min_ydistance=150, threshold=threshold)
+
+  return cy,cx, radii
+
+
+def detect_squares_nocirc(image_dir, ref=ref, thresh_square=70, thresh_circle=0.3):
+  x,y,_ = detect_circles(image_dir, threshold=thresh_circle) # Note, x = cy and y = cx for the detection because of the way images are read in.X here represents dist from top and y is dist from left
+
+  x1,y1,z = detect_squares(image_dir, ref=ref, threshold=thresh_square)
+  if x1 is None:
+    return [], []
+
+  x2,y2,z = circle_filter([x1,y1,z], [x,y], dist=150)
+  return x2,y2,z
+
+
+def disp_particles(im_dir, x, y):
+  img = cv2.imread(im_dir)
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+  img_blur = cv2.GaussianBlur(img, (5,5), 0)
+  im = cv2.Canny(image=img_blur, threshold1=100, threshold2=150)
+  plt.imshow(im)
+  plt.scatter(y,x, c='r')
+  plt.show()
+
+def snake_crop(im_dir, x, y, z, disp=True):
+  im = cv2.imread(im_dir)
+  im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+  crop_list = []
+  for i in np.arange(len(x)):
+    #print(max(0,x[i]-175), min(im.shape[0], x[i]+175), max(0,y[i]-175), max(im.shape[1],y[i]+175) )
+    t,b,l,r = max(0,x[i]-175), min(im.shape[0], x[i]+175), max(0,y[i]-175), min(im.shape[1],y[i]+175)
+    img = im[t:b,l:r]
+    #s = np.linspace(0, 2*np.pi, 400)
+    #r = 150 + 200*np.sin(s)
+    #c = 150 + 200*np.cos(s)
+    #init = np.array([r, c]).T
+    init = snake_list[z[i]]
+    mean = np.mean(init,axis=0, dtype=int)
+    init += [175,175] - mean #[175,175] is center of cropped region and also corresponds to the ping
+
+
+
+    snake = active_contour(gaussian(img, 3, preserve_range=False),
+                          init, alpha=0.03, beta=10, gamma=0.001) #alpha=0.015
+    crop_list.append((img,snake, init))
+    if disp:
+      fig, ax = plt.subplots(figsize=(7, 7))
+      ax.imshow(img, cmap=plt.cm.gray)
+      ax.plot(init[:, 1], init[:, 0], '--r', lw=3)
+      ax.scatter(snake[:, 1], snake[:, 0], c=np.arange(len(snake)), lw=3)
+      ax.set_xticks([]), ax.set_yticks([])
+      ax.axis([0, img.shape[1], img.shape[0], 0])
+
+      #plt.scatter(np.mean(snake,axis=0)[1], np.mean(snake,axis=0)[0])
+      plt.scatter(mean[0],mean[1],c='b')
+      plt.show()
+      dist = dist_center(snake)
+      plt.plot(dist)
+      peaks = find_peaks(dist, prominence = 3)
+      [plt.axvline(p, c='C3', linewidth=0.3) for p in peaks[0]]
+      plt.show()
+  return crop_list
+
+def circle_crop(im_dir,cx,cy,radii, disp=True):
+  image = cv2.imread(im_dir)
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  image = color.gray2rgb(image)
+  crop_list = []
+  for center_y,center_x,radius in zip(cy,cx,radii):
+    circy,circx = circle_perimeter(center_x,center_y,radius,shape=image.shape)
+    image[circy,circx] = (255,20,20)
+
+    t,b,l,r = max(0,center_x-radius), min(im.shape[0], center_x+radius), max(0,center_y-radius), min(im.shape[1],center_y+radius)
+    crop_list.append((image[t:b,l:r],[circx,circy]))
+
+    if disp:
+      fig, ax = plt.subplots(figsize=(7, 7))
+      ax.imshow(image[t:b,l:r])
+      plt.show()
+
+  return crop_list
+
+
+def dist_center(snake):
+  center = np.mean(snake,axis=0)
+  print(center)
+  dist = snake-center
+  print(dist[:5])
+  dist = np.sqrt(np.sum(dist**2,axis=1))
+  print(dist.shape)
+  return dist
+
+def save_detect(im_dir, save_dir, x_square, y_square, z_square, x_circle, y_circle, radii, segment=True, edge_crop=True):
+  im = cv2.imread(im_dir)
+  plt.figure(figsize=(10,10))
+  
+  if edge_crop:
+    edge_dist = 150
+    edge_mask = np.logical_or.reduce((y_circle<edge_dist, y_circle>height-edge_dist, x_circle<edge_dist, x_circle>width-edge_dist))
+    x_circle = x_circle[edge_mask]
+    y_circle = y_circle[edge_mask]
+
+    edge_mask = np.logical_or.reduce((y_square<edge_dist, y_square>height-edge_dist, x_square<edge_dist, x_square>width-edge_dist))
+    x_square = x_square[edge_mask]
+    y_square = y_square[edge_mask]
+
+  if segment:
+     
+     for center_y,center_x,radius in zip(y_circle,x_circle,radii):
+        circy,circx = circle_perimeter(center_x,center_y,radius,shape=im.shape)
+        plt.scatter(circx,circy, c='r')
+    
+     crop_list = snake_crop(im_dir,x_square,y_square, z_square, disp=False)
+     for i, (crop, snake, init) in enumerate(crop_list):
+        height,width = crop.shape
+        plt.plot(init[:, 1]+y_square[i]-width//2, init[:, 0]+x_square[i]-height//2, 'r', alpha=0.75)
+        plt.plot(snake[:, 1]+y_square[i]-width//2, snake[:, 0]+x_square[i]-height//2, 'b', alpha=0.75)
+        plt.scatter(np.mean(snake,axis=0)[1]+y_square[i]-width//2, np.mean(snake,axis=0)[0]+x_square[i]-height//2, c='g')
+ 
+  plt.imshow(im)
+  plt.scatter(y_circle,x_circle, c='r')
+
+  plt.scatter(y_square,x_square, c='b')
+
+  plt.savefig(save_dir)
+  plt.show()
+
